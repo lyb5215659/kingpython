b11d097c8c1959733d115775955a6c4c331230f4
lyb521569 <462966779@qq.com>
2019-11-15 10:12:59 +0800

    启动成功

--- www/app.py
+++ www/app.py
@@ -5,14 +5,15 @@ from datetime import datetime
 
 from aiohttp import web
 
-def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>')
 
-@asyncio.coroutine
-def init(loop):
-	app=web.Application(loop=loop)
+def index(request):
+	return web.Response(body=b'<h1>Awesome</h1>',headers={'content-type':'text/html'})
+
+async def init(loop):
+	app = web.Application(loop=loop)
+	# app=web_runner.AppRunner(app=app).app()
 	app.router.add_route('GET','/',index)
-	srv=yield from loop.create_server(app.make_handler(),'127.0.9.1',9000)
+	srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	logging.info('server started at http://127.0.0.1:9000')
 	return srv
 
